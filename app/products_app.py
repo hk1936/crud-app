bd7dbe6100ea30d4bc7b8d5089bb1c3edb1855e3
Hajime Kawanishi <hk1936@stern.nyu.edu>
2017-07-19 19:45:22 -0400

    Added definition and reading csv file

--- app/products_app.py
+++ app/products_app.py
@@ -1,30 +1,62 @@
 import os
+import csv
+csv_file_path = "/users/kawanishihajime/desktop/crud-app/data/products.csv"
 
 print("----------------------------")
 print("Products Application")
 print("----------------------------")
-
+print("Hello "+os.getlogin())
 menu = """
-Hello,
-
 Welcome to the product app!
 
 There are 100 products.
 
 Please choose an operation from:'List', 'Show', 'Create', 'Update', 'Destroy'
+
+When you are DONE, type in DONE!
 """
 print (menu)
-chosen_operation = input()
 
-if chosen_operation.title() == "List": #.title() if list is small letter, it will still capture
-    print ("Listing products")
-elif chosen_operation.title() == "Show":
-    print ("Showing A product")
-elif chosen_operation.title() == "Create":
-    print ("Creating A product")
-elif chosen_operation.title() == "Update":
-    print ("Updating A product")
-elif chosen_operation.title() == "Destroy":
-    print ("Destroying A product")
-else:
-    print("Operation not recognized. Choose appropriate operation from:'List', 'Show', 'Create', 'Update', 'Destroy'!")
+
+def list_products():
+    with open(csv_file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        print ("There are 20 products: \n") # assuming your CSV has headers, otherwise... csv.reader(csv_file)
+        for column in reader:
+            print(column["id"],column["name"],column["aisle"],column["department"], column["price"])
+
+def show_product():
+    print("Showing a product")
+
+def create_product():
+    print("Creating a product")
+
+def update_product():
+    print("Updating a product")
+
+
+def destroy_product():
+    print("Destroying a product")
+
+
+
+while True:
+    chosen_operation = input("Enter operation here: ")
+    chosen_operation = chosen_operation.title()
+    print ("You chose...."+chosen_operation)
+
+    if chosen_operation == "List": #.title() if list is small letter, it will still capture
+        list_products()
+    elif chosen_operation  == "Show":
+        show_product()
+    elif chosen_operation == "Create":
+        create_product()
+    elif chosen_operation == "Update":
+        update_product()
+    elif chosen_operation == "Destroy":
+        destroy_product()
+    elif chosen_operation.title() == "Done":
+        print ("Good bye!")
+        break
+    else:
+        print("Operation not recognized. Choose appropriate operation from:'List', 'Show', 'Create', 'Update', 'Destroy'!")
