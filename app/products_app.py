bb421a8fd89f8375adae10efe4651ae0819fabf4
Hajime Kawanishi <hk1936@stern.nyu.edu>
2017-07-19 18:43:50 -0400

    Step one commit

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,15 @@
+import os
 
+print("----------------------------")
+print("Products Application")
+print("----------------------------")
+
+menu = """
+Hello,
+Welcome to the product app!
+There are 100 products.
+Please choose an operation from:'List', 'Show', 'Create', 'Update', 'Destroy'
+"""
+
+print (menu)
+chosen_operation = input()
