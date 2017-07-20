ca46eabb7db07bf3f8c97f8b7bf8a1497fea3f39
Hajime Kawanishi <hk1936@stern.nyu.edu>
2017-07-19 21:06:58 -0400

    last update - worked on show, create

--- app/products_app.py
+++ app/products_app.py
@@ -8,6 +8,18 @@ with open(csv_file_path, "r") as csv_file:
     for row in reader:
         products.append(row)
 
+example_new_product = {"id": 21,"name":"New Item!", "aisle":"snacks","department":"ok","price": 3.55}
+products.append(example_new_product)
+
+other_path = "/users/kawanishihajime/desktop/crud-app/data/other_products.csv"
+with open(other_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle","department","price"])
+    writer.writeheader() # uses fieldnames set above
+    for product in products:
+        writer.writerow(product)
+
+
+
 print("----------------------------")
 print("Products Application")
 print("----------------------------")
@@ -20,7 +32,9 @@ There are {0} products.
 Please choose an operation from:'List', 'Show', 'Create', 'Update', 'Destroy'
 
 When you are DONE, type in DONE!
-""".format(len(products))
+""".format(len(products)) #string interporation
+
+
 
 print (menu)
 
@@ -31,13 +45,24 @@ def list_products():
         for row in reader:
             print(row["id"],row["name"],row["aisle"],row["department"], row["price"])
 
-
-
 def show_product():
-    print("Showing a product")
+	product_id = input("Please specify product IDs: ")
+
 
 def create_product():
-    print("Creating a product")
+    print("Ok, Please specify the product information....")
+    product_name = input("name: ")
+    product_aisle = input("aisle: ")
+    product_department = input("department: ")
+    product_price = input("price: ")
+    new_product = {
+    	"name": product_name,
+    	"aisle":product_aisle,
+    	"department":product_department,
+    	"price":product_price
+    }
+    print ("New product is", new_product)
+    products.append(new_product)
 
 def update_product():
     print("Updating a product")
@@ -45,7 +70,6 @@ def update_product():
 
 def destroy_product():
     print("Destroying a product")
-
 
 
 while True:
